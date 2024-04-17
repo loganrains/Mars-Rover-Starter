29d37fff092b56fc300b0dff23e61b81ee2aa7b4
loganrains <logankrains@gmail.com>
2024-04-17 15:18:17 -0500

    Completed Task 2

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -5,8 +5,17 @@ const Command = require('../command.js');
 
 describe("Command class", function() {
 
-  it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+    // Test 1
+  test("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
 
+    // Test 2
+  test("constructor sets command type", function() {
+    expect( new Command("x")).toEqual({"commandType": "x", "value": undefined});
+  });
+
+  test("constructor sets a value passed in as the 2nd argument", function() {
+    expect( new Command("x", "y")).toEqual({"commandType": "x", "value": "y"});
+  });
 });
