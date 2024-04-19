2d6a7b75eb4fd118021f1e3ddbd05e14d5fb5fd4
loganrains <logankrains@gmail.com>
2024-04-19 16:36:49 -0500

    Updated Message Specs

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -6,18 +6,21 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
-    // Test 4 <<<Rewrite>>>
+    // Test 4
     test("throws error if a name is NOT passed into the constructor as the first parameter", function() {
         expect( function() { new Message();}).toThrow(new Error("Name required."));
     });
 
-    // Test 5 <<<Rewrite>>>
+    // Test 5
     test("constructor sets name", function() {
-        expect( new Message("x")).toEqual({"name": "x", "commands": undefined});
+        expect( new Message("Test Message")).toEqual({"name": "Test Message", "commands": undefined});
     });
 
-    // Test 6 <<<Rewrite>>>
+    // Test 6
     test("contains a commands array passed into the constructor as the 2nd argument", function() {
-        expect( new Message("x", ["y", "z"])).toEqual({"name": "x", "commands": ["y", "z"]});
+        let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+        let message = new Message('Test Message', commands);
+        
+        expect(message.commands).toEqual([{"commandType": "MODE_CHANGE", "value": "LOW_POWER"}, {"commandType": "STATUS_CHECK", "value": undefined}]);
     });
 });
